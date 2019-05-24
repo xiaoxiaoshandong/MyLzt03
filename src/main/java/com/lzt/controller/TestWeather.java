fc0aa6a4d06d927e61f36f6d34810ec2b808e56e
Administrator <Administrator@CN-20120726MUQM>
2019-05-25 02:22:05 +0800

    3333

--- src/main/java/com/lzt/controller/TestWeather.java
+++ src/main/java/com/lzt/controller/TestWeather.java
@@ -3,30 +3,36 @@ package com.lzt.controller;
 import java.rmi.RemoteException;
 
 import javax.jws.WebService;
+import javax.xml.rpc.ServiceException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cn.com.WebXml.WeatherWebServiceLocator;
 import cn.com.WebXml.WeatherWebServiceSoapStub;
 
 @RestController  
 @RequestMapping("/weather") 
-@WebService
 public class TestWeather {
 	
 	
 	 // 调用获取支持查询某个省份内的城市接口
 	@RequestMapping("/invokeGetSupportCity") 
-    public static void invokeGetSupportCity(WeatherWebServiceSoapStub service)
-            throws RemoteException {
-        String provinceName = "江苏";
-        String[] cities = service.getSupportCity(provinceName);
-        System.out.println("总共" + cities.length + "个市");
-        for (int i = 0; i < cities.length; i++) {
-            if (0 != i && i % 5 == 0) {
-                System.out.println();
-            }
-            System.out.print(cities[i] + "\t");
+    public static void invokeGetSupportCity()
+            throws RemoteException, Exception {
+		WeatherWebServiceLocator locator = new WeatherWebServiceLocator();
+		 WeatherWebServiceSoapStub service = (WeatherWebServiceSoapStub) locator
+	                .getPort(WeatherWebServiceSoapStub.class);
+		invokeGetWeatherByOneCity(service);
+    }
+	
+	 // 调用查询某个城市天气的接口
+    public static void invokeGetWeatherByOneCity(
+            WeatherWebServiceSoapStub service) throws RemoteException {
+        String cityName = "苏州";
+        String[] weatherInfo = service.getWeatherbyCityName(cityName);
+        for (String str : weatherInfo) {
+            System.out.println(str);
         }
     }
 }
