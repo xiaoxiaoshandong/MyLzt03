1e14101fc841acf4bf81819992b78494919bb601
xiaoxiaoshandong <lizhentao_job@163.com>
2019-06-16 20:13:58 +0800

    aa

--- src/main/java/com/lzt/controller/B.java
+++ src/main/java/com/lzt/controller/B.java
@@ -1,12 +1,11 @@
 package com.lzt.controller;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class B {
-public static void main(String args[]) {
-int x=1,y=1,z=1;
-if (x--==1&&y++==1||z++==1)
-System.out.println("x="+x+",y="+y+",z="+z);
+	public static void main(String args[]) {
+		String[] s =new String[10];
+		System.out.println(s.length);
+	}
 }
-
-}
-
